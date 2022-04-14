6ed163b0c28ea388e117f3fcd1dd0a8e6974782c
Yura Perov <iury.perov@gmail.com>
2022-04-14 18:54:11 +0500

    O_o

--- src/app/internal/admin/bank/BankCardAdmin.py
+++ src/app/internal/admin/bank/BankCardAdmin.py
@@ -1,9 +1,15 @@
 from django.contrib.admin import ModelAdmin, register
 
-from app.internal.models.bank import BankCard
+from app.internal.models.bank import BankCard, Transaction
 
 
 @register(BankCard)
 class BankCardAdmin(ModelAdmin):
     list_display = ("pretty_number", "bank_account")
     exclude = ("number",)
+
+
+@register(Transaction)
+class TransactionAdmin(ModelAdmin):
+    list_display = ("source", "destination", "accrual")
+    
